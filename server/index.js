df122856ec9bf6e829c0f6b4b52cbb580b2a233c
jrjaro18 <rohanjaiswal1500@gmail.com>
2023-07-01 04:52:42 +0530

    Revert "nw"
    This reverts commit ecdb30b693e25bf7b5ef9da0d9c1115580d6919d.

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ require('./database');
 const app = express();
 
 const corsOptions = {
-  origin: ['https://ecom-vendora.netlify.app/','http://localhost:3000'],
+  origin: 'https://ecom-vendora.netlify.app/',
   credentials: true, // Enable sending cookies
 };
 app.use(cors(corsOptions));
